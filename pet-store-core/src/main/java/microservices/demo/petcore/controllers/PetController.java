95bff17be8ddd087fecc3ca2e0025afdd693897a
EstebanLcn <44468643+EstebanLcn@users.noreply.github.com>
2020-04-27 16:43:56 +0200

    Commit pour get by id et put pour l update

--- pet-store-core/src/main/java/microservices/demo/petcore/controllers/PetController.java
+++ pet-store-core/src/main/java/microservices/demo/petcore/controllers/PetController.java
@@ -1,20 +1,27 @@
 package microservices.demo.petcore.controllers;
 
 import io.micronaut.http.HttpResponse;
-import io.micronaut.http.MutableHttpResponse;
 import io.micronaut.http.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import javax.inject.Inject;
 
+import io.micronaut.management.endpoint.annotation.Endpoint;
 import microservices.demo.petcore.domains.dtos.PetDTO;
+import microservices.demo.petcore.domains.entities.Pet;
 import microservices.demo.petcore.services.PetService;
 
+@Endpoint(id = "id",
+          prefix = "custom",
+          defaultEnabled = true,
+          defaultSensitive = false)
 @Controller("/pet")
 public class PetController {
 
   @Inject
   private PetService service;
+
 
   @Get
   public List<PetDTO> retrievePets(){
@@ -26,5 +33,20 @@ public class PetController {
       service.deletePet(id);
         return HttpResponse.ok();
   }
+  /*@Post
+  public HttpResponse PostPet(@Body Pet pet){
+    service.PostPet(pet);
+    return HttpResponse.created(pet);
+  }*/
+  @Put
+  public HttpResponse PutPet(@Body Pet pet){
+      service.PutPet(pet);
+      return HttpResponse.ok();
+  }
+@Get(value ="/{id}")
+  public Optional<Pet> retrievePetsByID(Integer id){
+  return service.retrievePetsByID(id);
 
 }
+}
+
--- pet-store-core/src/main/java/microservices/demo/petcore/services/PetService.java
+++ pet-store-core/src/main/java/microservices/demo/petcore/services/PetService.java
@@ -1,14 +1,14 @@
 package microservices.demo.petcore.services;
 
-import io.reactivex.annotations.NonNull;
 import io.vavr.control.Try;
 import java.util.List;
+import java.util.Optional;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import lombok.extern.slf4j.Slf4j;
-import lombok.extern.slf4j.XSlf4j;
 import microservices.demo.petcore.domains.dtos.PetDTO;
+import microservices.demo.petcore.domains.entities.Pet;
 import microservices.demo.petcore.exceptions.ApiException;
 import microservices.demo.petcore.helpers.PetMapper;
 import microservices.demo.petcore.repositories.PetRepository;
@@ -20,6 +20,7 @@ public class PetService {
 
   @Inject
   private PetRepository repository;
+
 
   public List<PetDTO> retrievePets() {
     return Try.of(() -> repository.findAll())
@@ -52,5 +53,32 @@ public class PetService {
       log.error("unable to delete pet", e);
       throw new ApiException("Unable to delete pets", 500);
     }
+  }
+  public void PutPet(Pet pet) {
+
+    try {
+      repository.update(pet);
+    }
+
+    catch (RuntimeException e){
+      log.error("unable to update pet", e);
+      throw new ApiException("Unable to update pets", 500);
+    }
+  }
+  /*public void retrieveByIDPets(Integer id) {
+
+    try {
+      repository.findById(id);
+    } catch (RuntimeException e) {
+      log.error("unable to get id's pet", e);
+      throw new ApiException("Unable to get id's pets", 500);
+
+  }
+  }*/
+  public Optional<Pet> retrievePetsByID(Integer id) {
+
+      return repository.findById(id);
+
+
   }
 }
